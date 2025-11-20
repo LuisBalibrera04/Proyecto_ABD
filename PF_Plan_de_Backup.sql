6ddbafd888c43f9492f7d48e4feaa2b3faeb3d1b
Gerardo2006 <164081228+Gerardo2006@users.noreply.github.com>
2025-11-19 21:09:34 -0600

    Update PF_Plan_de_Backup.sql

--- PF_Plan_de_Backup.sql
+++ PF_Plan_de_Backup.sql
@@ -7,12 +7,19 @@ ALTER DATABASE GimnasioReservas SET RECOVERY FULL;
 
 BACKUP DATABASE GimnasioReservas
 TO DISK = 'C:\Backups\Gym_Full.bak'
-WITH COMPRESSION, NAME = 'GimnasioReserva full backup'
+WITH COMPRESSION, NAME = 'GimnasioReserva full backup', INIT;
 
---Restaurar desde el backup
-USE master
+--LOG backup
+BACKUP LOG GimnasioReservas
+TO DISK = 'C:\Backups\Gym_Cadena.trn'
+WITH NOINIT;
+
+-- 1. FULL
 RESTORE DATABASE GimnasioReservas
-FROM DISK = 'C:\Backups\Gym_Full.bak'
-WITH REPLACE, RECOVERY;
+FROM DISK = 'Gym_Full.bak' WITH REPLACE, NORECOVERY;
+
+-- 2. LOGs
+RESTORE LOG GimnasioReservas
+FROM DISK = 'Gym_Cadena.trn' WITH RECOVERY;
 
 SELECT name FROM sys.databases;
