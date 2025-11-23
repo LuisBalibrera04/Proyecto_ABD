a691269b2ab28eeaeb5b62c62481529e452f582d
Gerardo2006 <164081228+Gerardo2006@users.noreply.github.com>
2025-11-23 15:47:47 -0600

    Update PF_Plan_de_Backup.sql

--- PF_Plan_de_Backup.sql
+++ PF_Plan_de_Backup.sql
@@ -14,12 +14,27 @@ BACKUP LOG GimnasioReservas
 TO DISK = 'C:\Backups\Gym_Cadena.trn'
 WITH NOINIT;
 
+--Unico usuario
+ALTER DATABASE GimnasioReservas
+SET SINGLE_USER 
+WITH ROLLBACK IMMEDIATE;
+
 -- 1. FULL
 RESTORE DATABASE GimnasioReservas
-FROM DISK = 'Gym_Full.bak' WITH REPLACE, NORECOVERY;
+FROM DISK = 'C:\Backups\Gym_Full.bak' WITH REPLACE, NORECOVERY;
 
 -- 2. LOGs
 RESTORE LOG GimnasioReservas
-FROM DISK = 'Gym_Cadena.trn' WITH RECOVERY;
+FROM DISK = 'C:\Backups\Gym_Cadena.trn' WITH RECOVERY;
+
+--Vuelvo a multiusario
+ALTER DATABASE GimnasioReservas
+SET MULTI_USER;
 
 SELECT name FROM sys.databases;
+
+SELECT name, state_desc
+FROM sys.databases
+WHERE name = 'GimnasioReservas';
+
+EXEC Config.sp_ObtenerDimensionamientoTablas
